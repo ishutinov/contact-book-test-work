77f8a9fe25c84a020b78a9d7208f8961f2cf1826
Ishutinov Dmitry <ishutinov.dmitry@gmail.com>
2016-10-10 12:47:30 +0300

    add missing function

--- engine/core/app.php
+++ engine/core/app.php
@@ -4,5 +4,10 @@ class App {
 	static function security($var) {
 		return htmlspecialchars(trim($var));
 	}
+	# Check AJAX request
+	static function isAjax()
+	{
+	  return(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest');
+	}
 }
 ?>
